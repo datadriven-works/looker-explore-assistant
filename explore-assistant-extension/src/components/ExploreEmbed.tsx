33e1f9ea92618fcd35df0b1ff19ba00c90034a28
Flavio Di Berardino <flaviojdi@gmail.com>
2024-08-05 22:23:44 +0200

    fix issues after merging new main

--- explore-assistant-extension/src/components/ExploreEmbed.tsx
+++ explore-assistant-extension/src/components/ExploreEmbed.tsx
@@ -24,13 +24,12 @@ SOFTWARE.
 
 */
 
-
-import React, { useContext, useRef, useEffect } from 'react';
-import styled from 'styled-components';
-import { LookerEmbedSDK } from '@looker/embed-sdk';
-import { ExtensionContext } from '@looker/extension-sdk-react';
-import { useSelector } from 'react-redux';
-import { RootState } from '../store';
+import React, { useContext, useRef, useEffect } from 'react'
+import styled from 'styled-components'
+import { LookerEmbedSDK } from '@looker/embed-sdk'
+import { ExtensionContext } from '@looker/extension-sdk-react'
+import { useSelector } from 'react-redux'
+import { RootState } from '../store'
 
 export interface ExploreEmbedProps {
   exploreUrl: string
@@ -63,96 +62,85 @@ const processUrlParams = (exploreUrl: string): { [key: string]: string } => {
   return paramsObj;
 };
 
-export const ExploreEmbed = ({}: ExploreEmbedProps) => {
-  const { extensionSDK } = useContext(ExtensionContext);
-  const [exploreRunStart, setExploreRunStart] = React.useState(false);
 
-  const { exploreUrl, exploreId } = useSelector(
-    (state: RootState) => state.assistant,
-  );
+export const ExploreEmbed = ({ exploreUrl }: ExploreEmbedProps) => {
+  const { extensionSDK } = useContext(ExtensionContext)
+  const [exploreRunStart, setExploreRunStart] = React.useState(false)
+
+  const { exploreId } = useSelector((state: RootState) => state.assistant)
 
   const canceller = (event: any) => {
-    return { cancel: !event.modal };
-  };
+    return { cancel: !event.modal }
+  }
 
-  const ref = useRef<HTMLDivElement>(null);
+  const ref = useRef<HTMLDivElement>(null)
 
   const handleQueryError = () => {
-    setTimeout(() => !exploreRunStart && animateExploreLoad(), 10);
-  };
+    setTimeout(() => !exploreRunStart && animateExploreLoad(), 10)
+  }
 
   const animateExploreLoad = () => {
-    document.getElementById('embedcontainer')?.style.setProperty('opacity', '1');
-  };
+    document.getElementById('embedcontainer')?.style.setProperty('opacity', '1')
+  }
 
-  const setExploreLoading = (_explore: any) => {};
+  const setExploreLoading = (_explore: any) => {}
 
   useEffect(() => {
-    const hostUrl = extensionSDK?.lookerHostData?.hostUrl;
-    const el = ref.current;
+    const hostUrl = extensionSDK?.lookerHostData?.hostUrl
+    const el = ref.current
     if (el && hostUrl && exploreUrl) {
-      const paramsObj = {
-        embed_domain: hostUrl,
+      const baseParamsObj: { [key: string]: string } = {
+        // For Looker Original use window.origin for Looker Core use hostUrl
+        embed_domain: hostUrl, //window.origin, //hostUrl,
         sdk: '2',
         _theme: JSON.stringify({
           key_color: '#174ea6',
           background_color: '#f4f6fa',
         }),
-      };
-
-      console.log('exploreUrl before decoding:', exploreUrl);
+      }
 
       const decodedParams = processUrlParams(exploreUrl);
-      console.log('Decoded params:', decodedParams);
 
-      // Ensure specific fields are left as decoded without re-encoding
-      const finalParams: { [key: string]: string } = {};
-      for (const key in decodedParams) {
-        if (decodedParams.hasOwnProperty(key)) {
-          if (key.includes('filter_config') || key.includes('vis') || key.includes('fields') 
-              || key.startsWith('f[')) {
-            finalParams[key] = decodedParams[key]; // Do not re-encode JSON params, fields, or filters
-          } else {
-            finalParams[key] = encodeURIComponent(decodedParams[key]).replace(/%20/g, ' ');
-          }
-        }
-      }
-      console.log('Final processed params:', finalParams);
+      const paramsObj: { [key: string]: string } = {
+        ...baseParamsObj,
+        ...decodedParams,
+      };
 
-      el.innerHTML = '';
-      LookerEmbedSDK.init(hostUrl);
+      el.innerHTML = ''
+      LookerEmbedSDK.init(hostUrl)
       LookerEmbedSDK.createExploreWithId(exploreId)
         .appendTo(el)
         .withClassName('looker-embed')
-        .withParams(finalParams)
-        .on('explore:ready', handleQueryError)
+        .withParams(paramsObj)
+        .on('explore:ready', () => handleQueryError())
         .on('drillmenu:click', canceller)
         .on('drillmodal:explore', canceller)
         .on('explore:run:start', () => {
-          setExploreRunStart(true);
-          animateExploreLoad();
+          setExploreRunStart(true)
+          animateExploreLoad()
         })
         .on('explore:run:complete', () => setExploreRunStart(false))
         .build()
         .connect()
         .then((explore) => setExploreLoading(explore))
         .catch((error: Error) => {
-          console.error('Connection error', error);
-          // Additional debug information on error
-          console.error('Params Obj on Connection Error:', finalParams);
-        });
+          // @TODO - This should probably throw a visible error
+          // eslint-disable-next-line no-console
+          console.error('Connection error', error)
+        })
     }
-  }, [exploreUrl]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [exploreUrl])
 
   return (
     <>
       <EmbedContainer id="embedcontainer" ref={ref} />
     </>
-  );
-};
+  )
+}
 
 const EmbedContainer = styled.div<{}>`
-  background-color: #f7f7f7;
+  backgroundcolor: #f7f7f7;
   width: 100%;
   height: 100%;
   animation: fadeIn ease-in ease-out 3s;
@@ -160,6 +148,6 @@ const EmbedContainer = styled.div<{}>`
     width: 100%;
     height: 100%;
     display: block;
-    background-color: #f7f7f7;
+    backgroundcolor: #f7f7f7;
   }
-`;
+`
